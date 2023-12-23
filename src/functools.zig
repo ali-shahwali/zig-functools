6af0cb28f8bcc7357b67b1a1adcac4440759b174
Ali Shahwali <shahwali@kth.se>
2023-12-23 03:02:42 +0100

    Added util for common mappers, reducers and predicates.

--- src/functools.zig
+++ src/functools.zig
@@ -8,21 +8,183 @@ pub const FunctoolTypeError = error{
     InvalidReturnType,
 };
 
+/// Set of common mapping functions.
+pub const CommonMappers = struct {
+    /// Increment each number in slice.
+    pub fn inc(comptime T: type) fn (item: T) T {
+        return (struct {
+            fn apply(item: T) T {
+                return item + 1;
+            }
+        }).apply;
+    }
+
+    /// Decrement each number in slice.
+    pub fn dec(comptime T: type) fn (item: T) T {
+        return (struct {
+            fn apply(item: T) T {
+                return item - 1;
+            }
+        }).apply;
+    }
+
+    /// Add `n` to each item in slice. Supply `n` with the args.
+    pub fn add(comptime T: type) fn (item: T, n: T) T {
+        return (struct {
+            fn apply(item: T, n: T) T {
+                return item + n;
+            }
+        }).apply;
+    }
+
+    /// Subtract `n` to each item in slice. Supply `n` with the args.
+    pub fn sub(comptime T: type) fn (item: T, n: T) T {
+        return (struct {
+            fn apply(item: T, n: T) T {
+                return item - n;
+            }
+        }).apply;
+    }
+
+    /// Multiply `n` to each item in slice. Supply `n` with the args.
+    pub fn mul(comptime T: type) fn (item: T, n: T) T {
+        return (struct {
+            fn apply(item: T, n: T) T {
+                return item * n;
+            }
+        }).apply;
+    }
+
+    /// Divide `n` to each item in slice. Supply `n` with the args.
+    pub fn div(comptime T: type) fn (item: T, n: T) T {
+        return (struct {
+            fn apply(item: T, n: T) T {
+                return @divExact(item, n);
+            }
+        }).apply;
+    }
+
+    /// Strip item of all but one field supplied by args. The field type must also be provided.
+    pub fn takeField(comptime T: type, comptime FT: type) fn (item: T, comptime field: []const u8) FT {
+        return (struct {
+            fn apply(item: T, comptime field: []const u8) FT {
+                return @field(item, field);
+            }
+        }).apply;
+    }
+};
+
+/// Set of common reducers.
+pub const CommonReducers = struct {
+    /// Sum all numbers in slice.
+    pub fn sum(comptime T: type) fn (prev: T, curr: T) T {
+        return (struct {
+            fn apply(prev: T, curr: T) T {
+                return prev + curr;
+            }
+        }).apply;
+    }
+
+    /// Compute the product of all numbers in slice.
+    pub fn prod(comptime T: type) fn (prev: T, curr: T) T {
+        return (struct {
+            fn apply(prev: T, curr: T) T {
+                return prev * curr;
+            }
+        }).apply;
+    }
+};
+
+/// Set of common predicate functions.
+pub const CommonPredicates = struct {
+    /// Evalatues `true` if item is even.
+    pub fn even(comptime T: type) fn (item: T) bool {
+        return (struct {
+            fn apply(item: T) bool {
+                return @mod(item, 2) == 0;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if item is odd.
+    pub fn odd(comptime T: type) fn (item: T) bool {
+        return (struct {
+            fn apply(item: T) bool {
+                return @mod(item, 2) != 0;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item == 0`.
+    pub fn zero(comptime T: type) fn (item: T) bool {
+        return (struct {
+            fn apply(item: T) bool {
+                return item == 0;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item != 0`.
+    pub fn notZero(comptime T: type) fn (item: T) bool {
+        return (struct {
+            fn apply(item: T) bool {
+                return item != 0;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item` equals that of supplied in args. Only works on primitive types.
+    pub fn eq(comptime T: type) fn (item: T, n: T) bool {
+        return (struct {
+            fn apply(item: T, n: T) bool {
+                return item == n;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item` does not equal that of supplied in args. Only works on primitive types.
+    pub fn neq(comptime T: type) fn (item: T, n: T) bool {
+        return (struct {
+            fn apply(item: T, n: T) bool {
+                return item != n;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item` is `true`.
+    pub fn truthy() fn (item: bool) bool {
+        return (struct {
+            fn apply(item: bool) bool {
+                return item;
+            }
+        }).apply;
+    }
+
+    /// Evalatues `true` if `item` is `false`.
+    pub fn falsy() fn (item: bool) bool {
+        return (struct {
+            fn apply(item: bool) bool {
+                return !item;
+            }
+        }).apply;
+    }
+};
+
 /// Map over slice of type `T` to slice new slice of type `RT` using function `func` on each element of `slice`.
 /// Usually `T == RT`.
 /// Additionally supply some arguments to `func`,
-pub fn mapSlice(allocator: Allocator, comptime T: type, comptime RT: type, slice: []const T, comptime func: anytype, args: anytype) ![]RT {
+pub fn mapSlice(allocator: Allocator, comptime T: type, slice: []const T, comptime func: anytype, args: anytype) ![]@typeInfo(@TypeOf(func)).Fn.return_type.? {
     if (@typeInfo(@TypeOf(func)).Fn.params[0].type.? != T) {
         return FunctoolTypeError.InvalidParamType;
     }
-    if (@typeInfo(@TypeOf(func)).Fn.return_type.? != RT) {
-        return FunctoolTypeError.InvalidReturnType;
-    }
 
-    var mapped_list = try std.ArrayList(RT).initCapacity(allocator, slice.len);
+    const ReturnType = @typeInfo(@TypeOf(func)).Fn.return_type orelse {
+        return FunctoolTypeError.InvalidReturnType;
+    };
+    var mapped_list = try std.ArrayList(ReturnType).initCapacity(allocator, slice.len);
 
     for (0..slice.len) |idx| {
-        const mapped: RT = @call(.auto, func, .{slice[idx]} ++ args);
+        const mapped: ReturnType = @call(.auto, func, .{slice[idx]} ++ args);
         try mapped_list.append(mapped);
     }
 
@@ -113,4 +275,21 @@ pub fn everySlice(comptime T: type, slice: []const T, comptime pred: anytype, ar
     }
 
     return true;
+}
+
+/// Take every nth element in `slice` of type `T`.
+pub fn takeNth(allocator: Allocator, comptime T: type, slice: []const T, n: usize) ![]T {
+    if (n <= 0) {
+        return slice;
+    }
+
+    var nth = std.ArrayList(T).init(allocator);
+    for (0..slice.len) |idx| {
+        if (@mod(idx, n) == 0) {
+            try nth.append(slice[idx]);
+        }
+    }
+
+    const nth_slice = try nth.toOwnedSlice();
+    return nth_slice;
 }
--- src/tests.zig
+++ src/tests.zig
@@ -1,33 +1,24 @@
 const std = @import("std");
 const functools = @import("functools");
 const testing = std.testing;
+const CommonMappers = functools.CommonMappers;
+const CommonReducers = functools.CommonReducers;
+const CommonPredicates = functools.CommonPredicates;
 
 const Point2D = struct {
     x: i32,
     y: i32,
 };
 
-fn orthogonal(p1: Point2D, p2: Point2D) bool {
-    return (p1.x * p2.x + p1.y * p2.y) == 0;
-}
-
 fn sumPointY(prev: i32, curr: Point2D) i32 {
     return prev + curr.y;
 }
 
-fn sum(prev: i32, curr: i32) i32 {
-    return prev + curr;
-}
-
-fn inc(n: i32) i32 {
-    return n + 1;
-}
-
-test "test map slice on i32 slice without args" {
+test "test map on slice of type i32 to slice of type i64" {
     const allocator = testing.allocator;
 
     const slice = [3]i32{ 1, 2, 3 };
-    const incremented = try functools.mapSlice(allocator, i32, i64, &slice, (struct {
+    const incremented = try functools.mapSlice(allocator, i32, &slice, (struct {
         fn inci64(n: i32) i64 {
             return @as(i64, n + 1);
         }
@@ -39,11 +30,17 @@ test "test map slice on i32 slice without args" {
     try testing.expectEqual(incremented[2], 4);
 }
 
-test "test map on slice of type i32 to slice of type i64" {
+test "test map slice on i32 slice without args " {
     const allocator = testing.allocator;
 
     const slice = [3]i32{ 1, 2, 3 };
-    const incremented = try functools.mapSlice(allocator, i32, i32, &slice, inc, .{});
+    const incremented = try functools.mapSlice(
+        allocator,
+        i32,
+        &slice,
+        CommonMappers.inc(i32),
+        .{},
+    );
     defer allocator.free(incremented);
 
     try testing.expectEqual(incremented[0], 2);
@@ -54,23 +51,56 @@ test "test map on slice of type i32 to slice of type i64" {
 test "test map slice on i32 slice with args" {
     const allocator = testing.allocator;
     const slice = [3]i32{ 1, 2, 3 };
-    var outside_closure: i32 = 0;
-    const added = try functools.mapSlice(allocator, i32, i32, &slice, (struct {
-        fn add(x: i32, y: *i32) i32 {
-            y.* += 1;
-            return x + y.*;
-        }
-    }).add, .{&outside_closure});
+    const added: []i32 = try functools.mapSlice(
+        allocator,
+        i32,
+        &slice,
+        CommonMappers.add(i32),
+        .{@as(i32, 1)},
+    );
     defer allocator.free(added);
 
     try testing.expectEqual(added[0], 2);
-    try testing.expectEqual(added[1], 4);
-    try testing.expectEqual(added[2], 6);
+    try testing.expectEqual(added[1], 3);
+    try testing.expectEqual(added[2], 4);
+}
+
+test "test map slice on Point2D slice with takeField mapper" {
+    const allocator = testing.allocator;
+    const slice = [3]Point2D{ .{
+        .x = 1,
+        .y = 2,
+    }, .{
+        .x = 2,
+        .y = 3,
+    }, .{
+        .x = 3,
+        .y = 4,
+    } };
+    const x_coords: []i32 = try functools.mapSlice(
+        allocator,
+        Point2D,
+        &slice,
+        CommonMappers.takeField(Point2D, i32),
+        .{"x"},
+    );
+    defer allocator.free(x_coords);
+
+    try testing.expectEqual(x_coords[0], 1);
+    try testing.expectEqual(x_coords[1], 2);
+    try testing.expectEqual(x_coords[2], 3);
 }
 
 test "test reduce slice on i32 slice" {
     const slice = [3]i32{ 1, 2, 3 };
-    const result = try functools.reduceSlice(i32, i32, &slice, sum, .{}, 0);
+    const result = try functools.reduceSlice(
+        i32,
+        i32,
+        &slice,
+        CommonReducers.sum(i32),
+        .{},
+        0,
+    );
 
     try testing.expectEqual(result, 6);
 }
@@ -94,11 +124,13 @@ test "test reduce struct field" {
 test "test filter on i32 slice" {
     const slice = [5]i32{ 1, 2, 3, 4, 5 };
     const allocator = testing.allocator;
-    const even = try functools.filterSlice(allocator, i32, &slice, (struct {
-        fn pred(n: i32) bool {
-            return @mod(n, 2) == 0;
-        }
-    }).pred, .{});
+    const even = try functools.filterSlice(
+        allocator,
+        i32,
+        &slice,
+        CommonPredicates.even(i32),
+        .{},
+    );
     defer allocator.free(even);
 
     try testing.expectEqual(even[0], 2);
@@ -108,13 +140,18 @@ test "test filter on i32 slice" {
 
 test "test some on i32 slice" {
     const slice = [3]i32{ 1, 3, 5 };
-    const some_even = try functools.someSlice(i32, &slice, (struct {
-        fn pred(n: i32) bool {
-            return @mod(n, 2) == 0;
-        }
-    }).pred, .{});
+    const some_even = try functools.someSlice(
+        i32,
+        &slice,
+        CommonPredicates.even(i32),
+        .{},
+    );
 
     try testing.expect(!some_even);
+}
+
+fn orthogonal(p1: Point2D, p2: Point2D) bool {
+    return (p1.x * p2.x + p1.y * p2.y) == 0;
 }
 
 test "test some on Point2D slice" {
@@ -171,20 +208,13 @@ test "test wrong param type error" {
     const allocator = testing.allocator;
 
     const slice = [3]i64{ 1, 2, 3 };
-    _ = functools.mapSlice(allocator, i64, i64, &slice, inc, .{}) catch |err| {
+    _ = functools.mapSlice(
+        allocator,
+        i64,
+        &slice,
+        CommonMappers.inc(i32),
+        .{},
+    ) catch |err| {
         try testing.expect(err == functools.FunctoolTypeError.InvalidParamType);
-    };
-}
-
-test "test wrong return type error" {
-    const allocator = testing.allocator;
-
-    const slice = [3]i32{ 1, 2, 3 };
-    _ = functools.mapSlice(allocator, i32, i32, &slice, (struct {
-        fn inci64(n: i32) i64 {
-            return @as(i64, n + 1);
-        }
-    }).inci64, .{}) catch |err| {
-        try testing.expect(err == functools.FunctoolTypeError.InvalidReturnType);
     };
 }
