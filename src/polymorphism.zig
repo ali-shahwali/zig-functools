cf49237230c3d33c5f27bfa9492aac78aa58de14
Ali Shahwali <shahwali@kth.se>
2025-06-30 20:20:03 +0200

    add ad hoc polymorphism with return type utility function

--- src/polymorphism.zig
+++ src/polymorphism.zig
@@ -57,6 +57,61 @@ pub fn adHocPoly(args: anytype, fns: anytype) fn () typed.ReturnType(fns[0]) {
     }
 }
 
+pub fn adHocPolyT(comptime T: type, args: anytype, fns: anytype) fn () T {
+    const args_ty = @typeInfo(@TypeOf(args));
+    switch (args_ty) {
+        inline .@"struct" => |s| {
+            const fields = s.fields;
+            const fns_ty = @typeInfo(@TypeOf(fns));
+            switch (fns_ty) {
+                inline .@"struct" => |fns_struc| {
+                    for (fns_struc.fields, 0..) |func, fn_idx| {
+                        const fn_ty = @typeInfo(func.type);
+                        switch (fn_ty) {
+                            inline .@"fn" => |fn_ty_info| {
+                                if (fn_ty_info.params.len != fields.len) {
+                                    continue;
+                                }
+                                if (fn_ty_info.return_type) |RT| {
+                                    if (RT != T) {
+                                        continue;
+                                    }
+                                } else {
+                                    unreachable;
+                                }
+
+                                var found = true;
+                                for (fn_ty_info.params, 0..) |param, i| {
+                                    if (param.type) |param_ty| {
+                                        if (param_ty != fields[i].type) {
+                                            found = false;
+                                            break;
+                                        }
+                                    } else {
+                                        @compileError("generic fn parameter of anytype found, can not deduce which fn to call");
+                                    }
+                                }
+                                if (found) {
+                                    return (struct {
+                                        fn e() T {
+                                            return @call(.auto, fns[fn_idx], args);
+                                        }
+                                    }).e;
+                                }
+                            },
+                            else => @compileError("functions params was not an array of functions, was " ++ @typeName(@Type(fn_ty))),
+                        }
+                    }
+
+                    @compileError("no function which fulfills type signature of args was provided");
+                },
+                else => @compileError("functions was not a struct of fn, was " ++ @typeName(@Type(fns_ty))),
+            }
+        },
+        else => @compileError("expected struct for args, was " ++ @typeName(@Type(args))),
+    }
+}
+
 const Point2D = struct {
     x: i32,
     y: i32,
@@ -93,4 +148,25 @@ test "ad_hoc_polymorphism" {
         Point3D{ .x = 1, .y = 2, .z = 3 },
         Point3D{ .x = 4, .y = 5, .z = 6 },
     }));
+}
+
+fn @"*int_i64"(a: i64, b: i64) i64 {
+    return a * b;
+}
+
+fn @"*int_i32"(a: i64, b: i64) i32 {
+    return @intCast(a * b);
+}
+
+fn mul(comptime T: type, args: anytype) T {
+    return adHocPolyT(
+        T,
+        args,
+        .{ @"*int_i64", @"*int_i32" },
+    )();
+}
+
+test "ad_hoc_polymorphism_with_return_type" {
+    try testing.expectEqual(12, mul(i64, .{ @as(i64, 3), @as(i64, 4) }));
+    try testing.expectEqual(12, mul(i32, .{ @as(i64, 3), @as(i64, 4) }));
 }
